eb3381acee470a2503e3b4e37bc0e19066d3bec6
AxelRVS <Axel.rivas.p@gmail.com>
2026-03-17 22:16:53 +0100

    ajout calendrier.php

---
+++ calendrier.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "variable-connexion/config.php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calendrier</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="calendrier">
+    <?php require "html-commun/aside.html"; ?>
+    <div class="right-page">
+        <header>
+        <div class="filAriane">
+          <img src="assets/Home.svg" alt="Accueil" />
+          <p>></p>
+          <a href="">Calendrier</a>
+        </div>
+        <hr />
+      </header>
+      <main></main>
+    </div>
+</body>
+</html>
